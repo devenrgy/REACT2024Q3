64cbd301dbd7f9a02f59d2e6234e137c4e665627
Alex <alex.devenrgy@gmail.com>
2024-07-04 18:43:36 +0300

    feat: add workflows

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'https://rolling-scopes-school.github.io/devenrgy-REACT2024Q3/',
 });
