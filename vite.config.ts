6edc9529663a0e0acb359fa1aff908410e22a764
Alex <alex.devenrgy@gmail.com>
2024-07-04 18:39:03 +0300

    fix: configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	base: 'https://rolling-scopes-school.github.io/devenrgy-REACT2024Q3/',
 });
